3fb472393ec830cb3710560ddb447a08b9188f6c
root <mareikedatascience@gmail.com>
2021-01-23 17:30:16 +0000

    Update tokenize function

--- app/run.py
+++ app/run.py
@@ -19,15 +19,42 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 def tokenize(text):
+    '''
+    Transform text messages (documents) into normalized & lemmatized tokens.
+    Normalization steps are:
+    1. Lowercase
+    2. Replace URLs by placeholders
+    3. Replace punctuation
+    
+    Input arguments:
+    text - A single text message following a disaster
+    
+    Output:
+    lem_tokens - Normalized & lemmatized tokens
+    '''
+    # 1. normalize lowercase
+    text = text.lower()
+    
+    # 2. replace URLs by placeholder
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, 'urlplaceholder')
+        
+    # 3. normalize to digits and numbers only (replace by whitespace)
+    text = re.sub(r'[^a-zA-Z0-9]',' ',text)
+    
+    # word tokenize
     tokens = word_tokenize(text)
+    
+    # lemmatize token by token
     lemmatizer = WordNetLemmatizer()
+    lem_tokens = []
+    for i in tokens:
+        lem_tok = lemmatizer.lemmatize(i).strip()
+        lem_tokens.append(lem_tok)
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
+    return lem_tokens
 
 class SentenceCountExtractor(BaseEstimator, TransformerMixin):
     '''
