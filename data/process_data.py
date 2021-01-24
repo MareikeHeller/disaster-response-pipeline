eb0f37b9e6c3cde02f702963d15292ebc3b5ec8f
root <mareikedatascience@gmail.com>
2021-01-24 12:04:53 +0000

    Upload data & processing file

--- data/process_data.py
+++ data/process_data.py
@@ -17,15 +17,15 @@ def load_data(messages_filepath, categories_filepath):
     # load messages
     messages = pd.read_csv(messages_filepath)
     print('Loaded messages successfully.')
-    
+
     # load categories
     categories = pd.read_csv(categories_filepath)
     print('Loaded categories successfully.')
-    
+
     # merge dataframes
     df = messages.merge(categories, how='left', on='id')
     return df
-    
+
 
 def clean_data(df):
     '''
@@ -42,32 +42,32 @@ def clean_data(df):
     Output:
     df - Dataframe of merged messages and categories after cleaning
     '''
-    
+
     # 1. create a dataframe of the individual category columns
     categories = df['categories'].str.split(';', expand=True)
-    
+
     # 2. rename column names for categories
-    row = categories.iloc[0,:]
+    row = categories.iloc[0, :]
     category_colnames = row.apply(lambda x: x[:-2])
     categories.columns = category_colnames
-    
+
     # 3. clean label information for all categories
     # set each value to be the last character of the string
     # if last character > 1, set to 1
     for column in categories:
         categories[column] = categories[column].str[-1:].apply(lambda x: 1 if (int(x) > 1) else x)
         categories[column] = pd.to_numeric(categories[column])
-    
+
     # 4. replace original column 'categories' by individual columns per category
-    df = df.drop('categories',axis=1)
-    df = pd.concat([df,categories], axis=1)
-    
+    df = df.drop('categories', axis=1)
+    df = pd.concat([df, categories], axis=1)
+
     # 5. drop duplicate rows based on 'message'
-    print(str(df[df.duplicated(subset=['message'])==True].shape[0]) + ' duplicate rows.')
+    print(str(df[df.duplicated(subset=['message']) == True].shape[0]) + ' duplicate rows.')
     print('Dropping duplicate rows...')
-    df = df.drop_duplicates(subset=['message'],keep='first')
-    print(str(df[df.duplicated(subset=['message'])==True].shape[0]) + ' duplicate rows.')
-    
+    df = df.drop_duplicates(subset=['message'], keep='first')
+    print(str(df[df.duplicated(subset=['message']) == True].shape[0]) + ' duplicate rows.')
+
     return df
 
 
@@ -94,18 +94,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
               'DisasterResponse.db')
 
 
